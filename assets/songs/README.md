1d3fdcea47a68d6e8fa02ddf833cf8c194a8384b
Rodrigo Heinzen <rodrigo.h.m.112200@gmail.com>
2024-10-22 14:50:27 -0300

    init

---
+++ assets/songs/README.md
@@ -0,0 +1,1 @@
+All the songs in this folder were created using [Suno](https://suno.com)'s Generative AI and have no copyrighyt attached. Feel free to use them as you see fit.
---
+++ songs.js
@@ -0,0 +1,46 @@
+const ALL_SONGS = [
+  "Chill in the Blue",
+  "Cold Colors and Nature's Call",
+  "One Last Dance",
+  "Whiskey Tears",
+];
+
+const currentSongs = [...ALL_SONGS];
+
+/**
+ * @type {HTMLAudioElement}
+ */
+let audio;
+
+function play() {
+  if (!currentSongs.length) {
+    currentSongs.push(...ALL_SONGS);
+  }
+  const [randomSong] = currentSongs.splice(
+    Math.floor(Math.random() * currentSongs.length),
+    1
+  );
+  document
+    .querySelector("#music-namer")
+    .querySelector("a").innerText = `${randomSong}`;
+  audio = new Audio(`./assets/songs/${randomSong}.mp3`);
+  audio.volume = 0.1;
+  audio.play();
+  audio.onended = () => {
+    setTimeout(play, 1000);
+  };
+}
+
+document.querySelector("#play-button").addEventListener("click", (e) => {
+  e.preventDefault();
+  document
+    .querySelector("#music-namer")
+    .querySelector("small")
+    .classList.toggle("d-none");
+  if (!audio) {
+    play();
+  } else {
+    audio.pause();
+    audio = null;
+  }
+});
